a976f0faa74f4ae2a8fc7ad39f2b7f15fb3bf4e3
erkanturker <erkanturker17@gmail.com>
2020-04-21 11:24:31 -0400

    I just add testing file to examine if it is working or not

--- src/day_33/Test.java
+++ src/day_33/Test.java
@@ -3,5 +3,6 @@ package day_33;
 public class Test {
     public static void main (String[] args) {
         System.out.println ("This is the test for testing git");
+        System.out.println ("This is will be my second practice on test Java");
     }
 }
