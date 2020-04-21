b6b66a2a45533ad5866cdddc536a6db44c7cb973
erkanturker <erkanturker17@gmail.com>
2020-04-21 11:28:33 -0400

    This is the 3 one

--- src/day_33/Test.java
+++ src/day_33/Test.java
@@ -4,5 +4,6 @@ public class Test {
     public static void main (String[] args) {
         System.out.println ("This is the test for testing git");
         System.out.println ("This is will be my second practice on test Java");
+        System.out.println ("This is 3rd one");
     }
 }
