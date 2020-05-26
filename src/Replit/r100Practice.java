e3f8d492015c186ffcef4663421de42bda6101bd
erkanturker <erkanturker17@gmail.com>
2020-05-26 11:20:04 -0400

    Simple date modifier was added

--- src/Replit/r100Practice.java
+++ src/Replit/r100Practice.java
@@ -1,8 +1,13 @@
 package Replit;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class r100Practice {
     public static void main (String[] args) {
         System.out.println ("practive");
+        SimpleDateFormat dateFormat = new SimpleDateFormat ( "yyyy-mm-dd" );
+        System.out.println (dateFormat.format ( new Date (  ) ));
 
     }
 }
